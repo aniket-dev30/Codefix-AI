2b0468827ffee8aec044af290042d0e4227bcbfd
Aniket Jha <134406663+aniket-dev30@users.noreply.github.com>
2026-04-30 20:20:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     try {
       console.log("Button clicked");
 
-      const res = await axios.post("http://localhost:5000/debug", {
+      const res = await axios.post("https://codefix-ai-dn7x.onrender.com", {
         code,
         language: "javascript",
       });
